ecf335c19cf2765a972f794882f0afa37710654e
MaGo1024 <34481013+MaGo1024@users.noreply.github.com>
2019-07-01 19:57:08 +0100

    Installation and Creation Express App

---
+++ server.js
@@ -0,0 +1,12 @@
+// var http =  require("http");
+
+// http.createServer(function(){
+//     res.write("Hello Client");
+//     res.end();
+// }).listen(3000, function() {
+//     console.log("Server started at port 3000");
+// });
+
+
+const express = require("express");
+const app = express();
